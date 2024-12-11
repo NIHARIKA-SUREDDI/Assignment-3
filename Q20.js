165118d9c538b0a5a55fdd2ec823472a6540adc1
NIHARIKA-SUREDDI <niharika.sureddi@gmail.com>
2024-12-11 10:33:28 +0530

    closures

--- Q20.js
+++ Q20.js
@@ -1,3 +1,5 @@
+
+//Question 1
 function createCounter() {
     let count = 0; 
     return {
@@ -19,3 +21,38 @@ let  counter = createCounter();
 counter.increment(); 
 counter.increment(); 
 counter.decrement(); 
+
+//----------------------------------------------------------------------------------------------
+//Question 2
+
+function createBankAccount() {
+    let balance = 0; 
+
+    return {
+        deposit(amount) {
+            if (amount > 0) balance += amount;
+            console.log(`Deposited: ${amount}`);
+        },
+        withdraw(amount) {
+            if (amount > 0 && amount <= balance) {
+                balance -= amount;
+                console.log(`Withdrawn: ${amount}`);
+            } else {
+                console.log('Insufficient balance');
+            }
+        },
+        checkBalance() {
+            console.log(`Current balance: ${balance}`);
+        }
+    };
+}
+
+let account = createBankAccount();
+account.deposit(500); 
+account.withdraw(200); 
+account.withdraw(400); 
+account.checkBalance(); 
+
+
+
+
